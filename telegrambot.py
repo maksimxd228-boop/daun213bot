54faacefc1f89a73d8dd30e07ccf2c06a921d29e
maksimxd228-boop <maksims.nesvetovs@gmail.com>
2026-08-26 14:38:58 +0300

    Update telegrambot.py

--- telegrambot.py
+++ telegrambot.py
@@ -1,17 +1,16 @@
+import os
 import telebot
 
-# ВСТАВЬ СЮДА НОВЫЙ ТОКЕН ПОСЛЕ REVOKE
-TOKEN = "8961094590:AAHB8Z8CEvC1gpMpB7sdjiFuiO7yDroYoVw"
-
+TOKEN = os.environ.get("TOKEN")
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Я живой! Daun213Bot запущен.")
+    bot.send_message(message.chat.id, "Я живой! daun213bot запущен.")
 
 @bot.message_handler(func=lambda m: True)
 def echo(message):
     bot.send_message(message.chat.id, f"Ты написал: {message.text}")
 
-print("Бот запущен! Не закрывай это окно. Иди в Телегу и пиши /start")
+print("Бот запущен! Напиши в Телегу /start")
 bot.infinity_polling()
