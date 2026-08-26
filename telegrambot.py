827c6d69a57a7ba7df10b8373874472bb558cd67
maksimxd228-boop <maksims.nesvetovs@gmail.com>
2026-08-26 14:46:07 +0300

    Update telegrambot.py

--- telegrambot.py
+++ telegrambot.py
@@ -1,16 +1,28 @@
 import os
 import telebot
+import threading
+from flask import Flask
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
+
+threading.Thread(target=run_web).start()
 
 TOKEN = os.environ.get("TOKEN")
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Я живой! daun213bot запущен.")
+    bot.send_message(message.chat.id, "Я живой 24/7!")
 
 @bot.message_handler(func=lambda m: True)
 def echo(message):
     bot.send_message(message.chat.id, f"Ты написал: {message.text}")
 
-print("Бот запущен! Напиши в Телегу /start")
 bot.infinity_polling()
